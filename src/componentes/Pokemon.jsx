b9cd252ea62f721fa9b5c7b5c92dc2dcab92b496
Victor9i <paulovgomes23@gmail.com>
2023-02-25 15:41:08 -0300

    feat: add props to Pokemon

--- src/componentes/Pokemon.jsx
+++ src/componentes/Pokemon.jsx
@@ -1,5 +1,7 @@
-export const Pokemon = () => {
-    return( 
-     <h2>Nome do pokemon:Pikachun </h2>
+
+export const Pokemon = ({name = 'Pikacun', tipo}) => {
+
+ return( 
+     <h2>Nome do pokemon:{name}, Tipo do Pokemon: {tipo} </h2>
     )
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,11 @@ const Home = () => {
   return (
     <div>
       <h1>Meu site de pokemon</h1>
-         <Pokemon/>
+         <Pokemon name="pikachu" tipo="Raio" />
+         <Pokemon name="Charizad" tipo ="terra"/>
+         <Pokemon name=" Squirtiz" tipo ="vento"/>
+         <Pokemon name=" Dito" tipo ="fogo"/>
+   
          </div>
 )
 }
